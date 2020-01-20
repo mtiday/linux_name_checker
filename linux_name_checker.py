56424722071053a835e7f3e65ee6e95d687991aa
Michael Tiday <mtiday@gmail.com>
2020-01-19 21:56:21 -0700

    Add files via upload
    Made changes to the file locally, then saved over the previous file.

--- linux_name_checker.py
+++ linux_name_checker.py
@@ -1,21 +1,19 @@
-''' file name checker for Windows
+"""file name checker for Windows
 
 This program crawls through a directory and lets you know if there are
 names that will be considered duplicates in Windows. i.e. two folders named
 example and Example in Linux would be the same folder in Windows. Therefore
 if you copied this directory from Linux to Windows not everything would
 copy. I have had issues with SpiderOak backing up files from Linux to Windows
-and visa-versa. 
-
+and visa-versa.
 Michael Tiday mtiday@tidayventures.com
-'''
+"""
 
 import os
-#Create Global variables
-GLOBAL_DIRECTORY_TO_SCAN = ''
+
 
 def start():
-    '''This function starts the program. Gets the directory to scan.'''
+    """This function starts the program. Gets the directory to scan."""
     # Create a loop so the user has unlimited attempts to get the path correct
     while True:
         print('\nPlease enter a directory to scan for names that '
@@ -23,85 +21,87 @@ def start():
               '\nOr simply enter D to scan your Documents folder.'
               '\nEnter \'Q\' to quit the program:'
               '\n\nPlease note: It may take a while to scan large directories, like "home"\n')
-        directory_to_scan= str(input())
+        directory_to_scan = str(input())
 
-        # Q to end program
+        # 'Q' Raises SystemExit() to end program
         if directory_to_scan in ('q', 'Q'):
             print('\nHave a great day!!!\n')
             raise SystemExit()
 
         # D to  to select Documents folder
         if directory_to_scan in ('d', 'D'):
-            directory_to_scan= os.path.expanduser('~/Documents')
+            directory_to_scan = os.path.expanduser('~/Documents')
 
         # Verify valid path was entered, if valid call the crawler
         try:
             os.chdir(directory_to_scan)
-            global GLOBAL_DIRECTORY_TO_SCAN
-            GLOBAL_DIRECTORY_TO_SCAN = directory_to_scan
             directory_crawler(directory_to_scan)
+        # Raise exception if directory isn't valid
         except FileNotFoundError:
             print(f"\n'{directory_to_scan}' is not a valid path\nPlease try again.")
 
+
 def directory_crawler(top_folder):
-    '''Builds a list of file and directory names for duplicate checking.
-    
+    """Builds a list of file and directory names for duplicate checking.
     This function does the 'work' it will scan the directory and sub-directories
-    
     :param str top_folder: filesystem path to begin working in
 
     Raises:
-    - SystemExit on completion
+    - SystemExit on completion"""
 
-    '''
-    # create the needed lists:
     # list of all scanned directories and files
     directories_and_files = []
-    # list that will contain all scanned directories 
-    check_for_duplicates = []
+    # Contents of this list will be lower case
+    directories_and_files_lower = []
+    # list will contain files Windows would consider duplicates
     duplicates = []
 
-    #Navigate to directory to scan
+    # Navigate to directory to scan
     os.chdir(top_folder)
-    print('\nChecking for name issues in "' + os.getcwd() +'"\n')
+    print('\nChecking for name issues in "' + os.getcwd() + '"\n')
 
     for path, dirnames, filenames in os.walk('.'):
-        #Add directories to list
+        # Add directories to list
         for subdirname in dirnames:
-            check_for_duplicates.append(os.path.join(path, subdirname))
+            directories_and_files.append(os.path.join(path, subdirname))
 
-        #Add files to list
+        # Add files to list
         for filename in filenames:
-            check_for_duplicates.append(os.path.join(path, filename))
+            directories_and_files.append(os.path.join(path, filename))
 
-    #Make everything in the list the same case
-    for entry in directories_and_files:
-        if list_to_check_for_duplicates.count(entry) > 1:
+    # Make everything in the list the same case
+    for path in directories_and_files:
+        directories_and_files_lower.append(path.lower())
+
+    # Create a list of that Windows would consider duplicates
+    for entry, lc_entry in zip(directories_and_files, directories_and_files_lower):
+        if directories_and_files_lower.count(lc_entry) > 1:
             duplicates.append(entry)
     if len(duplicates) > 0:
-        build_desktop_file(duplicates)
+        build_desktop_file(duplicates, top_folder)
+    else:
+        print('There were no names found that would conflict in Windows')
+        print('Have a great day!!!\n')
     raise SystemExit()
 
-def build_desktop_file(list_to_check):
-    '''Given a list of "Windows" duplicate names, saves the list to 
+
+def build_desktop_file(duplicates, directory_to_scan):
+    """Given a list of "Windows" duplicate names, will save the list to
     a file on the Desktop.
-    
-    :param list list_to_check:
-    '''
-    #clean list a little, removing '.'
-    list_to_check = [entries.replace('.', '"\n"') for entries in list_to_check]
-    # Create file on Desktop with possible duplicates, if applicable
-    if len(list_to_check) > 0:
-        os.chdir(os.path.expanduser('~/Desktop'))
-        with open('Problem name(s) in Windows.txt', 'w') as problem_names:
-            list_to_string = ' '.join([str(entry) for entry in list_to_check])
-            problem_names.write(GLOBAL_DIRECTORY_TO_SCAN)
-            problem_names.write(list_to_string)
-        print('A file named "Problem name(s) in Windows.txt" has been saved on your Desktop')
-    else:
-        print('There were no names that would conflict in Windows')
+    :param:
+    list duplicates: any duplicates will be stored here
+    directory_to_scan: Folder that was scanned to be saved in .txt file
+    """
+    os.chdir(os.path.expanduser('~/Desktop'))
+    with open('Problem name(s) in Windows.txt', 'w') as problem_names:
+        problem_names.write(f'The conflict(s) below were found scanning directory '
+                            f'{directory_to_scan}')
+        for element in duplicates:
+            problem_names.write('\n')
+            problem_names.write(element)
+        print('A file named "Problem name(s) in Windows.txt" has been saved on your Desktop\n')
 
 
-#start the program
+# start the program
 if __name__ == "__main__":
     start()
